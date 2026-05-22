ab3fa197d43d3eee418f66573c7f0afc23f61550
Vinícius Ribeiro <viniciuspr2812@gmail.com>
2026-05-21 23:14:34 -0300

    feat(builder): implementa métodos de construção em TrilhaAprendizagemBuilder

--- frontend/src/lib/builder.js
+++ frontend/src/lib/builder.js
@@ -24,4 +24,41 @@ class TrilhaAprendizagemBuilder {
   constructor() {
     this.reset();
   }
+
+  reset() {
+    this.trilha = new TrilhaAprendizagem();
+    return this;
+  }
+
+  setId(id) {
+    this.trilha.id = id;
+    return this;
+  }
+
+  setNome(nome) {
+    this.trilha.nome = nome;
+    return this;
+  }
+
+  setObjetivo(objetivo) {
+    this.trilha.objetivo = objetivo;
+    return this;
+  }
+
+  setNivel(nivel) {
+    this.trilha.nivel = nivel;
+    return this;
+  }
+
+  // Builder delega ao método do produto
+  comModulo(modulo) {
+    this.trilha.adicionarModulo(modulo);
+    return this;
+  }
+
+  build() {
+    const resultado = this.trilha;
+    this.reset();
+    return resultado;
+  }
 }
